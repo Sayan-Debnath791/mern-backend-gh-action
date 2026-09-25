4b8be0d228dc90df42d40443c1f6b285e21471cb
Sayan <debnathsayan791@gmail.com>
2026-09-25 22:27:13 +0530

    frontend add

--- src/server.js
+++ src/server.js
@@ -13,16 +13,8 @@ const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http
 app.use(helmet());
 app.use(morgan('combined'));
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-      return;
-    }
-
-    callback(new Error('Not allowed by CORS'));
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
+  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+  credentials: true
 }));
 app.use(express.json());
 
